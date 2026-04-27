a4bee0a39de51cfdca4a0a43265a8975b1f73cfa
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2026-04-27 17:42:34 -0600

    feat: configure global settings for environment variables and database connection
    - Added ConfigModule for global configuration management.
    - Integrated TypeOrmModule for PostgreSQL database connection with environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
